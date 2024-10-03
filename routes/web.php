f9510414b29f2bf91bf97067ee58db4924e3b0f5
TROLLKOPF <max.serratosa@gmail.com>
2024-10-03 12:00:17 +0200

    added Get method to external download

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [XmlToXlsController::class, 'index']);
 Route::post('/convert', [XmlToXlsController::class, 'convert'])->name('convert');
+Route::get('/convert', [XmlToXlsController::class, 'convert'])->name('convert');
